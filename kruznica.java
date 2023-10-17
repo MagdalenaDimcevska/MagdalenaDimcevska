9db7afddd1f78f2d3483b0d5298ed5980c9386b4
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2023-10-17 15:00:01 +0200

    Add files via upload

---
+++ kruznica.java
@@ -0,0 +1,21 @@
+package proekt_1;
+
+import java.util.Scanner;
+
+public class kruznica {
+		public static void main(String[] args) {
+			double p,l;
+			System.out.println("Programata presmetuva perimetar i plostina na kruznica");
+			Scanner tastatura = new Scanner(System.in);
+			double r = tastatura.nextDouble();
+			System.out.println("radium e"+r+"cm");
+			double pi = tastatura.nextDouble();
+			System.out.println("pi e"+pi+"cm");
+			System.out.println();
+			p=r*r*pi;
+			System.out.println("plostinata e"+p);
+			l=2*r*pi;
+			System.out.println("perimetarot e"+l);
+	}
+
+}
